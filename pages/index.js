4e263a22a5809a53f903a213dcaf53bea0aa4a48
calito-351 <calito351@gmail.com>
2026-04-09 11:27:17 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
+
 export default function Home() {
-  return <h1>App SaaS a funcionar</h1>;
+  const [status, setStatus] = useState("A ligar...");
+
+  useEffect(() => {
+    async function test() {
+      const { data, error } = await supabase.auth.getSession();
+
+      if (error) {
+        setStatus("Erro ligação");
+      } else {
+        setStatus("Ligado ao Supabase");
+      }
+    }
+
+    test();
+  }, []);
+
+  return <h1>{status}</h1>;
 }
